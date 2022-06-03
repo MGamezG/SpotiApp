91cd78695fcbff7eb85107ebf3730583e3666aa0
MGamezG <l171130314@gamadero.tecnm.mx>
2022-06-02 20:02:40 -0500

    Http Intro

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  paises:any[]=[];
 
-  constructor() { }
+  /**
+   *
+   * @param http parametro que debe devolver en el metodo get
+   */
+  constructor(private http:HttpClient) {//http, permite tener todas las propiedades y metodos que contenga httpclient
+
+    console.log('constructor de home')
+    this.http.get('https://restcountries.com/v3.1/lang/spa') // esto es una peticion get
+    .subscribe((resp:any)=>{//informacion en bruto que debuelve el servicio
+      this.paises=resp
+      console.log(resp);// imprime toda la informacion que contenga la URl en consola
+    });
+   }
 
   ngOnInit(): void {
   }

b360186c9b1989b052c0e893b09dcaa6c58188e6
MGamezG <l171130314@gamadero.tecnm.mx>
2022-06-05 16:46:30 -0500

    search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent  {
+  artistas:any[]=[];
 
-  constructor() { }
+  constructor( private SpotifyService:SpotifyService) { }
 
-  ngOnInit(): void {
+  /**
+   * buscar todas las coincidencias
+   * @param termino
+   */
+  buscar(termino:string){
+    console.log(termino);
+    this.SpotifyService.getArtist(termino)
+    .subscribe((data:any)=>{
+      console.log(data.artists.items);
+      this.artistas=data.artists.items;
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,12 +17,24 @@ export class SpotifyService {
     */
    getNewReleases(){
      const headers=new HttpHeaders({//esto permite espesificar los header que necesita la app, en este caso son la autorizacion y el token
-       'Authorization':'Bearer BQCN-vV6DKiV61HSIf_-KIjjdz1_EDcyFn0w6g0KKoMovTnmEAF9-KmcjuiI7ttYnTMQgy78KIEP4L7ZPKo'// si el token caduca, solo generar un get en postman para obtener nuevo token
+       'Authorization':'Bearer BQCWEYkNvJIjSx0iQFjRwUK5V9QRxFEMnfJqjfoxK81foes2zuuDq6Z7ql-b_K_oerncvCRO9yRngh7XEws'// si el token caduca, solo generar un get en postman para obtener nuevo token
      });
     return this.http.get('https://api.spotify.com/v1/browse/new-releases',{headers});//peticion al url
     //en este caso pide un token, por lo que es necesario proveer headers
     // .subscribe(data=>{
     //   console.log(data);// imprime la infromacion del servicio
     // });
+  }
+
+  /**
+   *regresa una lista de artistas
+   * @param termino artista
+   * @returns obesrvable<any>
+   */
+  getArtist(termino:string):Observable<any>{
+    const headers=new HttpHeaders({
+      'Authorization':'Bearer BQCWEYkNvJIjSx0iQFjRwUK5V9QRxFEMnfJqjfoxK81foes2zuuDq6Z7ql-b_K_oerncvCRO9yRngh7XEws'
+    });
+   return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`,{headers});
   }
 }
